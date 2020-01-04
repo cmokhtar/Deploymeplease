ce253ac206c5710d1ae9f3e1136ef6f836ecb7e5
cmokhtar <53994312+cmokhtar@users.noreply.github.com>
2020-01-04 15:22:37 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,1 +1,10 @@
-print("HALLO")
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+    
